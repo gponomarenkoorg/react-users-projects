c9d3ce58ae1379a02cac20937da50d8628c07683
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-08-09 13:05:54 +0300

    first commit with api dir

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react';
-// eslint-disable-next-line no-unused-vars
+import localUsers from './api/localUsers.json';
 import { getUser, request } from './api/api';
 import './App.scss';
 
 const App = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [users, setUsers] = useState([]);
-  // const [selectedUserId, selectUserId] = useState(0);
-  // const [user, setUser] = useState(null);
+  const [users, setUsers] = useState(localUsers);
+  const [selectedLogin, selectLogin] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // eslint-disable-next-line no-console
     console.log('Mounted');
 
     getUser('defunkt')
-      // eslint-disable-next-line no-console
       .then(res => console.log(res));
 
     // request('GET /users/')
     //   .then(res => setUsers(res));
     // console.log(users);
   }, []);
+
+
 
   return (
     <h1>Users list</h1>
